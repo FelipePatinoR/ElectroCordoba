00f261c382d1dc7201f0091dec6cbddde3ed3d00
Dark66612 <cordobalopezjhon666@gmail.com>
2023-08-21 18:40:42 -0500

    perfil.3

--- app/Views/Perfil/perfil.php
+++ app/Views/Perfil/perfil.php
@@ -5,15 +5,13 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="card">
-                        <img src="imagen_de_perfil.jpg" class="card-img-top" alt="Imagen de Perfil">
                         <div class="card-body">
                             <h5 class="card-title">Nombre de Usuario</h5>
-                            <p class="card-text">Descripción del usuario.</p>
                         </div>
                         <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Telefono: 3125488548</li>
                             <li class="list-group-item">Correo: correo@example.com</li>
                             <li class="list-group-item">Ubicación: Ciudad, País</li>
-                            <li class="list-group-item">Sitio Web: <a href="https://www.ejemplo.com">www.ejemplo.com</a></li>
                         </ul>
                     </div>
                 </div>
