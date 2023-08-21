52783f2f68f43babce6d10142969e8333f53f7e7
Dark66612 <cordobalopezjhon666@gmail.com>
2023-08-21 15:56:19 -0500

    perfil.1

--- app/Views/Perfil/perfil.php
+++ app/Views/Perfil/perfil.php
@@ -1,6 +1,32 @@
 <?php echo $this->extend('index'); ?>
 <?php echo $this->section('content'); ?>
 <div>
-hola
+
+<div class="container d-flex justify-content-center align-items-center" style="min-height: 100vh;">
+    <!-- Contenido de tu contenedor -->
+
+
+
+<div class="container mt-5 custom-container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <img src="imagen_de_perfil.jpg" class="card-img-top" alt="Imagen de Perfil">
+                <div class="card-body">
+                    <h5 class="card-title">Nombre de Usuario</h5>
+                    <p class="card-text">Descripción del usuario.</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Correo: correo@example.com</li>
+                    <li class="list-group-item">Ubicación: Ciudad, País</li>
+                    <li class="list-group-item">Sitio Web: <a href="https://www.ejemplo.com">www.ejemplo.com</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>
+
 </div>
 <?php echo $this->endSection(); ?>
