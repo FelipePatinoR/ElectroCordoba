a42c6f041c3696135ed4bcdc17bb97c2c6f90eee
Dark66612 <cordobalopezjhon666@gmail.com>
2023-08-24 22:50:14 -0500

    aliniado

--- app/Views/A_acerca/acercade.php
+++ app/Views/A_acerca/acercade.php
@@ -5,7 +5,7 @@
             <div class="col-md-10">
                 <center>
                 <h1 class="text-white">Acerca de Nosotros</h1><br>
-                <p class="text-white">En <strong>Nombre de la Tienda</strong>, somos tu destino confiable para todas tus necesidades de herramientas y suministros de ferretería. Desde nuestra fundación en <strong>2014</strong>
+                <p class="text-white">En <strong>ElectroCordoba</strong>, somos tu destino confiable para todas tus necesidades de herramientas y suministros de ferretería. Desde nuestra fundación en <strong>2014</strong>
                  hemos estado comprometidos en proporcionar a nuestros clientes productos de alta calidad, atención al cliente excepcional y un servicio que puedes confiar. Nuestra pasión por la construcción y la mejora del hogar nos impulsa a ofrecerte una amplia gama de productos cuidadosamente seleccionados, 
                  desde herramientas manuales hasta maquinaria especializada. Ya sea que seas un profesional experimentado o un entusiasta del bricolaje, estamos aquí para ayudarte a llevar a cabo tus proyectos con éxito. ¡Únete a nuestra comunidad de clientes satisfechos y descubre por qué somos la opción número uno en ferretería en línea!</p>
                  </center>
